3ad2afbe748a02c649b22d1a527ab718fa0dd009
Roman Hrydin <153761577+Romario198901@users.noreply.github.com>
2024-11-21 14:25:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//Створити телефонну книгу
+//створити початковий клас Abonent, де зберігатимуться ім*я і номер
+//створити set який прийматиме телефон і номер
+//створити get який виводитиме данні про абонента
+//створити три різних юзери
+//вивести данні
+
+/*class Phonebook {
+    constructor(abonents) {
+        this.abonents = abonents;
+    }
+}
+class Abonent {
+    constructor(props) {
+        this.name = props.name;
+    };
+    contactsName = "";
+    contactsNumber = "";
+        set name(newContact) {
+            const nameRow = newContact.split(' ');
+            this.contactsName = nameRow[0];
+            this.contactsNumber = nameRow[1];
+        };
+        get name () {
+            return ` : ${this.contactsName} :  ${this.contactsNumber}`
+        };
+}
+
+const Roman = new Abonent({
+    name: 'Roman +380987028897', 
+});
+console.log(Roman);
+const Olha  = new Abonent ({
+    name:  'Olha +380972144434',
+});
+console.log(Olha);
+const Mayre = new Abonent ({
+    name: 'Mayre +380994230933',
+});
+console.log(Mayre);*/
